c071890b05d757a1a88a440b9de3dc36a4048956
MaxenceGui <96267006+MaxenceGui@users.noreply.github.com>
2022-10-29 21:26:16 -0400

    Commencer LinkedList
    Utilisation des fonction __iter__, __next__ et __eq__ pour réussir à override for

--- dev/app/app.py
+++ dev/app/app.py
@@ -1,12 +1,18 @@
 import datetime as dt
 from serveur_api import Serveur
 from modele.Modele import GestionSysteme, Enregistrement
+from modele.Liste_chainee import DoubleLinkedList
 
 
 def main(): 
-    gs = GestionSysteme()
-    Serveur.definir_controleur(gs)
-    Serveur.demarrer_serveur()
+    list = DoubleLinkedList() # 1, 2, 3, 4
+    list.add_first(3)
+    list.add_first('2')
+    list.add_first(1)
+    print(list.get_last()._data)
+    # gs = GestionSysteme()
+    # Serveur.definir_controleur(gs)
+    # Serveur.demarrer_serveur()
     
     
 if __name__=='__main__':
