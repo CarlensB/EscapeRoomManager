369d7ecc1e2924af74f2b672ff640e4a0fd5a695
Carlens Belony <carlens2000@live.ca>
2022-10-31 14:35:05 -0400

    dropdownmenu fonctinne pas

--- dev/FrontEnd/src/Comps/CreerCentreComp.tsx
+++ dev/FrontEnd/src/Comps/CreerCentreComp.tsx
@@ -19,44 +19,105 @@ const Formulaire = observer(() => {
         React.createElement(genererCentres)
     )
 
+    function listeCentre(){
+      let liste = []
+        for (let i = 0; i<accueilStore.compagnie.centres.length; i++)
+          {
+            liste.push(React.createElement('option',
+            {value:accueilStore.compagnie.centres[i].nom},
+            accueilStore.compagnie.centres[i].nom))
+          }  
+       
+      return liste
+    }
+
     function genererCentres(){
 
         let errMessage = ""
-        if (accueilStore.compagnie.newCentreInfos.nom.length < 1 && accueilStore.compagnie.newCentreInfos.adresse.length < 1){
-        errMessage = "Il manque des informations";
-        }
+        
+        // if (accueilStore.compagnie.newCentreInfos.nom.length < 1 && accueilStore.compagnie.newCentreInfos.adresse.length < 1){
+        // errMessage = "Il manque des informations";
+        // }
 
+        
+        
         
         return(React.createElement('div',
         {class:'ContainerFormulaire'},
+
         React.createElement(
-            'label',
-            {for: 'nom'},
-            'Nom: '),
-    
+          'div',
+          {class:'nouveauCentreFormulaire'},
+
           React.createElement(
-            'input',
-            {type: 'text', name: 'nom', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
-            null),
-    
+              'label',
+              {for: 'nom'},
+              'Nom: '),
+      
+            React.createElement(
+              'input',
+              {type: 'text', name: 'nom', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
+              null),
+      
+            React.createElement(
+              'label',
+              {for: 'adresse'},
+              'Adresse:'),
+          
+            React.createElement(
+              'input',
+              {type: 'text', name: 'adresse', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
+              null),
+      
+            React.createElement(
+              'div',
+              {class:"error_msg"},
+              errMessage
+          ),
           React.createElement(
-            'label',
-            {for: 'adresse'},
-            'Adresse:'),
-        
-          React.createElement(
-            'input',
-            {type: 'text', name: 'adresse', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
-            null),
-    
-          React.createElement(
-            'div',
-            {class:"error_msg"},
-            errMessage
+            'button',
+            {class:"submit_button"},
+            'Ajouter une succursale')
         ),
-        )
-        )
         
+
+        React.createElement(
+          'div',
+          {class:'ModifierCentreFormulaire'},
+
+          React.createElement('select',
+          {name:'centreChoix'},
+          React.createElement(listeCentre),
+          ),
+
+          React.createElement(
+              'label',
+              {for: 'nom'},
+              'Nom: '),
+      
+            React.createElement(
+              'input',
+              {type: 'text', name: 'nom', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
+              null),
+      
+            React.createElement(
+              'label',
+              {for: 'adresse'},
+              'Adresse:'),
+          
+            React.createElement(
+              'input',
+              {type: 'text', name: 'adresse', onChange:evt => {accueilStore.compagnie.newCentreInfos.nom = evt.currentTarget.value}},
+              null),
+      
+            React.createElement(
+              'div',
+              {class:"error_msg"},
+              errMessage
+          ),
+        ))
+
+      ) 
     }
   }
 )
