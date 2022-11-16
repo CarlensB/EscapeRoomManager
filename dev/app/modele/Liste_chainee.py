b0cd1753be1c7bd0e118b9e6164302b9a4ffdbc5
1475093 <maxence.guindon@outlook.com>
2022-11-16 15:12:55 -0500

    Liste double chainée finalisé

--- dev/app/modele/Liste_chainee.py
+++ dev/app/modele/Liste_chainee.py
@@ -8,13 +8,37 @@ class DoubleLinkedList:
             self._data = data
             self._next_node = None
             self._prev_node = None
+            
+        @property
+        def value(self) -> any:
+            return self._data
+            
+        @property
+        def next_val(self) -> any:
+            return self._next_node
+        
+        @property
+        def prev_val(self) -> any:
+            return self._prev_node
+        
+        @value.setter
+        def value(self, value) -> any:
+            self._data = value
+        
+        @next_val.setter
+        def next_val(self, value: any) -> None:
+            self._next_node = value
+            
+        @prev_val.setter
+        def prev_val(self, value: any) -> None:
+            self._prev_node = value
 
         def __eq__(self, node: 'DoubleLinkedList._Node') -> bool:
             if isinstance(node, DoubleLinkedList._Node):
-                return self._data == node._data
+                return self.value == node.value
         
         def __repr__(self) -> str:
-            return str(self._data)
+            return str(self.value)
         
         # Source pour la fonction __eq__
         # Source : https://www.pythontutorial.net/python-oop/python-__eq__/
@@ -34,14 +58,38 @@ class DoubleLinkedList:
     def __next__(self) -> 'DoubleLinkedList._Node':
         if self.n != None:
             result = self.n
-            self.n = self.n._next_node
+            self.n = self.n.next_val
             return result
         else:
-            raise StopIteration
+            raise StopIteration               
 
     # source __iter__ et __next__
     # Source : https://www.programiz.com/python-programming/iterator
     # Titre : Building Custom Iterators
+    
+    def __reversed__(self) -> 'DoubleLinkedList._Node':
+        reverse_list = []
+        self.prev = self.__last_node
+        while self.prev != None:
+            reverse_list.append(self.prev)
+            self.prev = self.prev.prev_val
+        return reverse_list
+    
+    # inspiration pour reversed :
+    # https://stackoverflow.com/questions/21529359/reversing-a-linked-list-in-python
+    
+    def __getitem__(self, index: int) -> any:
+        if isinstance(index, int):
+            if index < 0:
+                index = len(self) + index
+
+            for i, value in enumerate(self):
+                if i == index:
+                    return value
+            raise IndexError(f'{type(self).__name__} index {index} out of range(0, {len(self)-1})')
+        
+    # source pour getitem:
+    # https://stackoverflow.com/questions/57400399/implementation-of-linkedlist-in-python-getitem-method
 
     def __repr__(self) -> str:
         if self.is_empty:
@@ -50,9 +98,9 @@ class DoubleLinkedList:
         node = self.__front_node
         while node is not None:
             res += repr(node)
-            if node._next_node is not None:
+            if node.next_val is not None:
                 res += ", "
-            node = node._next_node
+            node = node.next_val
         res += "]"
         return res
 
@@ -92,12 +140,12 @@ class DoubleLinkedList:
         new_node = self._Node(new_data)
         if not self.is_empty:
             self.__verify_data((new_data,))
-            self.__front_node._prev_node = new_node
-            if self.__front_node._next_node is None:
+            self.__front_node.prev_val = new_node
+            if self.__front_node.next_val is None:
                 self.__last_node = self.__front_node
         else:
             self.__determine_data_type(new_data)
-        new_node._next_node = self.__front_node
+        new_node.next_val = self.__front_node
         self.__front_node = new_node
         self.__lenght += 1
     
@@ -109,12 +157,12 @@ class DoubleLinkedList:
         if self.is_empty:
             self.__determine_data_type(new_data)
             self.__front_node = new_node
-            if self.__front_node._next_node is None:
+            if self.__front_node.next_val is None:
                 self.__last_node = self.__front_node
         else:
             self.__verify_data((new_data,))
-            self.__last_node._next_node = new_node
-            new_node._prev_node = self.__last_node
+            self.__last_node.next_val = new_node
+            new_node.prev_val = self.__last_node
             self.__last_node = new_node
         self.__lenght += 1
         
@@ -132,11 +180,11 @@ class DoubleLinkedList:
 
             try:
                 search_node = self.__search_node(prev_data)
-                new_node._prev_node = search_node
-                new_node._next_node = search_node._next_node
+                new_node.prev_val = search_node
+                new_node.next_val = search_node.next_val
                 if search_node is not self.__last_node:
-                    search_node._next_node._prev_node = new_node
-                search_node._next_node = new_node
+                    search_node.next_val.prev_val = new_node
+                search_node.next_val = new_node
                 self.__last_node = new_node if search_node is self.__last_node else self.__last_node
                 self.__lenght += 1
 
@@ -149,19 +197,20 @@ class DoubleLinkedList:
     def remove(self, data: any) -> None:
         if not self.is_empty:
             self.__verify_data((data,))
-            if data is self.__front_node._data:
+            if data is self.__front_node.value:
                 self.remove_firt()
                 return
 
-            if data is self.__last_node._data:
+            if data is self.__last_node.value:
                 self.remove_last()
                 return
 
             search_node = self.__search_node(data)
 
-            search_node._next_node._prev_node = search_node._prev_node
-            search_node._prev_node._next_node = search_node._next_node
+            search_node.next_val.prev_val = search_node.prev_val
+            search_node.prev_val.next_val = search_node.next_val
             del(search_node)
+            self.__lenght -= 1
 
         else:
             raise ValueError(f"The list is empty")
@@ -171,20 +220,17 @@ class DoubleLinkedList:
 
     def remove_firt(self) -> None:
         node = self.__front_node
-        self.__front_node = self.__front_node._next_node
-        self.__front_node._prev_node = None
+        self.__front_node = self.__front_node.next_val
+        self.__front_node.prev_val = None
         del(node)
         self.__lenght -= 1
 
     def remove_last(self) -> None:
         node = self.__last_node
-        self.__last_node = self.__last_node._prev_node
-        self.__last_node._next_node = None
+        self.__last_node = self.__last_node.prev_val
+        self.__last_node.next_val = None
         del(node)
         self.__lenght -= 1
-    
-    def get_value(self):
-        pass
 
     def __place_value(self, value: tuple) -> None:
         self.__determine_data_type(value[0])
@@ -205,10 +251,10 @@ class DoubleLinkedList:
         # Source : https://stackoverflow.com/questions/54163163/python-while-with-two-conditions-and-or-or
         first = self.__front_node
         last = self.__last_node
-        while first._data != data and last._data != data:
-            first = first._next_node
-            last = last._prev_node
-        search_node = first if last._data != data else last
+        while first.value != data and last.value != data:
+            first = first.next_val
+            last = last.prev_val
+        search_node = first if last.value != data else last
         return search_node
 
             
@@ -229,4 +275,7 @@ if __name__ == '__main__':
     list.remove(21)
     list.remove(19)
     print(list)
+    print(reversed(list))
+    print(reversed(list)[5])
+    print(list[5])
    
