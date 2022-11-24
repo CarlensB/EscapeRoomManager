eb2c8e901ead9faa8aab5645dffd22ff07ff98fe
1475093 <maxence.guindon@outlook.com>
2022-11-24 13:02:26 -0500

    Possible d'avoir l'horaire pour plusieurs salles avec différentes contraintes

--- dev/app/modele/Algorithme.py
+++ dev/app/modele/Algorithme.py
@@ -14,24 +14,48 @@ class AlgoContext():
         
     def create_schedule(self, h_start: float, h_end: float, duration: float = 60., interval: float = 15., **kwargs):
         '''
+        Receive the time in minutes for the h_start and h_end variable.
         Can also build a schedule for multiple room. Accept these key-values argument:
         nb_room : int
         nb_emp: int
         reset_time: float
         greeting_time: float
+        goodbye_time: float
+        buffer: float
+        The buffer variable is build from reset_time, greeting_time and goodbye_time if it's
+        not received by the function.
+        h_end is receive here as the last start of the activity.
         '''
         
         keys = set(kwargs.keys())
         if keys == set():
             self.__strategy_naive.execute_algorithme(h_start, h_end, duration, interval)
-        elif keys == {'nb_room'}:
-            pass
-        elif keys == {'nb_room', 'nb_emp'}:
-            pass
-        elif keys == {'nb_room', 'reset_time'}:
-            pass
-        elif keys == {}:
-            pass
+            return self.__strategy_naive.schedule
+            
+        elif keys == {'nb_room', 'buffer'}:
+            self.__strategy_bc.execute_algorithme(h_start, h_end, kwargs['nb_room'], duration, interval, kwargs['buffer'])
+            return self.__strategy_bc.schedule
+            
+        elif keys == {'nb_room', 'greeting_time', 'goodbye_time'}:
+            buffer = kwargs['greeting_time'] + kwargs['goodbye_time']
+            self.__strategy_bc.execute_algorithme(h_start, h_end, kwargs['nb_room'], duration, interval, buffer)
+            return self.__strategy_bc.schedule
+            
+        elif keys == {'nb_room', 'reset_time', 'goodbye_time'}:
+            buffer = kwargs['reset_time'] + kwargs['goodbye_time']
+            self.__strategy_bc.execute_algorithme(h_start, h_end, kwargs['nb_room'], duration, interval, buffer)
+            return self.__strategy_bc.schedule
+            
+        elif keys == {'nb_room', 'reset_time', 'greeting_time'}:
+            buffer = kwargs['reset_time'] + kwargs['greeting_time']
+            self.__strategy_bc.execute_algorithme(h_start, h_end, kwargs['nb_room'], duration, interval, buffer)
+            return self.__strategy_bc.schedule
+            
+        elif keys == {'nb_room', 'reset_time', 'greeting_time', 'goodbye_time'}:
+            buffer = kwargs['reset_time'] + kwargs['greeting_time'] + kwargs['goodbye_time']
+            self.__strategy_bc.execute_algorithme(h_start, h_end, kwargs['nb_room'], duration, interval, buffer)
+            return self.__strategy_bc.schedule
+            
         elif keys == {'nb_room', 'nb_emp', 'reset_time'}:
             pass
         elif keys == {'reset_time', 'greeting_time'}:
@@ -53,9 +77,7 @@ class AlgoStrategy(ABC):
     def __init__(self) -> None:
         super().__init__()
         self.__schedule = []
-        self.h_start = None
-        self.h_end = None
-        self.__space_between_start = None
+        self._space_between_start = None
         
     @property
     def schedule(self):
@@ -103,25 +125,28 @@ class AlgoNaive(AlgoStrategy):
     def __init__(self) -> None:
         super().__init__()
 
-    def ajust_schedule_end(self):
-        self.h_end = self.h_end + (24*60)
+    def ajust_schedule_end(sel, h_end: float):
+        return h_end + (24*60)
             
-    def create_schedule(self):
-        next_start = self.h_start
-        while next_start <= self.h_end:
-            self.schedule.append(next_start)
-            next_start = self.schedule[-1] + self.__space_between_start
-        self.schedule = [i/60 for i in a.schedule]
-        self.schedule = [i-24 if i > 24 else i for i in a.schedule]
+    def create_schedule(self, h_start: float, h_end: float):
+        schedule = []
+        next_start = h_start
+        while next_start <= h_end:
+            schedule.append(next_start)
+            next_start = schedule[-1] + self._space_between_start
+        schedule = [i/60 for i in schedule]
+        schedule = [i-24 if i > 24 else i for i in schedule]
+        return schedule
         
-    def execute_algorithme(self, h_start: float, h_end: float, duration: int=60, interval: int=30):
-        self.h_start = h_start
-        self.h_end = h_end
-        self.__space_between_start = duration + interval
+    def execute_algorithme(self, h_start: float, h_end: float, duration: float=60., interval: float=30.):
+        '''
+        The start and end time need to be in minute for the function to work.
+        '''
+        self._space_between_start = duration + interval
         
-        if self.h_start > self.h_end:
-            self.ajust_schedule_end()
-        self.create_schedule()
+        if h_start > h_end:
+            h_end = self.ajust_schedule_end(h_end)
+        self.schedule = self.create_schedule(h_start, h_end)
 
 #...
 
@@ -150,7 +175,20 @@ class BackChecking(AlgoNaive):
     def ratio(self, value):
         self.__ratio = value
         
-    def execute_algorithme(self, a: list, n: int, k: int, buffer: float, depth: int = 0, used: list= [], ans: list = [], curr: list = []):
+    def execute_algorithme(self, h_start: float, h_end: float, nb_salle: int, duration: float = 60., interval: float = 30., buffer: float = 15.):
+        self._space_between_start = duration + interval
+        
+        a = [h_start]*nb_salle
+        n = len(a)
+        a = self.execute_A_n_k(a, n, n, buffer, used=[False]*n)
+
+        for i in range(n):
+            if a[i] > h_end:
+                h_end = self.ajust_schedule_end(h_end)
+            self.schedule.append((self.create_schedule(a[i], h_end), i))
+
+        
+    def execute_A_n_k(self, a: list, n: int, k: int, buffer: float, depth: int = 0, used: list= [], ans: list = [], curr: list = []):
         '''
         Implement permutation of k items out of n items
         depth: start from 0, and represent the depth of the search
@@ -160,37 +198,30 @@ class BackChecking(AlgoNaive):
         '''
         if depth == k:
             ans.append(curr[::])
-            print('test : ', [i/60 for i in a])
             return
         
         for i in range(n):
             if not used[i]:
                 curr.append(a[i])
                 used[i] = True
-                # Contraintes d'intervalle
+                # Contraintes de salle et départ différent (1 salles d'accueil)
                 # Contraintes d'employés (inclus le temps de rangement et ou temps d'accueil)
-                if self.ratio is not None and self.ratio < 1:
-                    if a[i] - a[i-1] < buffer:
-                        a[i] += buffer
-                    # try:
-                    #     prev = a[i-1]
-                    # except:
-                    #     prev = -1
-                    # if prev == a[i]:
-                    #     a[i] += interval
-                    # a[i] = buffer                       
-                
-                print(curr)
-                self.execute_algorithme(a, n, k, buffer, depth+1, used, ans, curr)
+                a[i] = self.validate_value_unique(a[i], i, buffer, a)         
+
+                self.execute_A_n_k(a, n, k, buffer, depth+1, used, ans, curr)
                 
                 curr.pop()
-                print('backtrack: ', curr)
-                used[i] = False
-                
-        return
+                used[i] = False 
+        return a
+    
+    def validate_value_unique(self, value: any, index:any, buffer: any, tab: list):
+        for idx, elem in enumerate(tab):
+            if value == elem and index != idx:
+                value += buffer
+        return value
 
 class FowardChecking:
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self) -> None:
         pass
 
 #...
@@ -206,7 +237,5 @@ class MinConflict:
  # +-+-+-+-+
 
 if __name__ == '__main__':
-    a = BackChecking()#AlgoNaive(duration=45, h_start=9*60, h_end=1*60, intervalle=15)
-    a.ratio = 1/3
-    a.execute_algorithme([9*60, 9*60, 9*60], 3, 3, 15, used=[False]*3)
-    #print(a.schedule)
+    s = AlgoContext()
+    print(s.create_schedule(8.*60, 21.*60, 90, 30, nb_room=3, greeting_time=15, goodbye_time=15))
